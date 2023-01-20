bd32d50687da16aab7e00ca58b81eb09217f22ae
OlegKhudoerko <110164546+OlegKhudoerko@users.noreply.github.com>
2023-01-20 13:36:09 +0300

    Update Logg.java

--- src/oop_lesson5/Calc/Logg.java
+++ src/oop_lesson5/Calc/Logg.java
@@ -3,7 +3,7 @@ package oop_lesson5.Calc;
 import java.util.logging.Logger;
 
 public class Logg {
-     public static final Logger logger = Logger.getLogger("jjj");
+     public static final Logger logger = Logger.getLogger("Пора застрелиться...");
 }
 
 
